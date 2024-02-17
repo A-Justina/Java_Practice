69784bd1d0ff3178083f01dcd746cc810797a4b0
Justina <k.justina@hotmail.com>
2024-02-16 23:00:57 -0600

    Day 12

---
+++ src/day12_encapsulation_tasks/CandyClients.java
@@ -0,0 +1,16 @@
+package day12_encapsulation_tasks;
+
+public class CandyClients {
+
+    public static void main(String[] args) {
+
+        Candy candy1 = new Candy("Mars", 2, 2.2, true);
+        System.out.println(candy1);
+        System.out.println("===================================");
+
+        Candy candy2 = new Candy("Snickers", 1, 0,true);
+        System.out.println(candy2);
+
+
+    }
+}
